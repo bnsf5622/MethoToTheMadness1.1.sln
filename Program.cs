36de386fa314ea593a923d0727a54ea2c9171957
Chuck Francis <bnsf5622@gmail.com>
2020-01-15 20:55:08 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MethodToTheMadness1._1.sln
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EatMe();
+
+            Console.WriteLine("\n ? Lewis Carroll, Alice in Wonderland");
+        }
+
+        static void EatMe()
+        {
+            Console.WriteLine("'But I don't want to go among mad people,' Alic remark.");
+            Console.WriteLine("'Oh, you can't help that,' said the cat ");
+            Console.Write("'Oh, you can't help that,' said the Cat.");
+        }
+
+        static void DrinkMe()
+        {
+            Console.WriteLine("'How do you know I'm mad?' said Alice.");
+            Console.WriteLine("'You must be,' said the Cat, 'or you wouldn't have come here.");
+        }
+    }
+}
